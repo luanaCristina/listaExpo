b2915f269468904635045a04ee42a3b880b8ae55
Luana CristinaCroft <luanacrystinne@msn.com>
2023-09-12 23:46:02 -0300

    atualizando o código

--- AppForms.js
+++ AppForms.js
@@ -1,19 +1,21 @@
-import { StatusBar } from "expo-status-bar"; 
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-export default function AppForm(){
-    return(
-        <View style = {style.container}>
-            <Text>Formulário!</Text>
-            <StatusBar style="light"/>
-        </View>
-    );
-    const style = StyleSheet({
-       container: {
-        flex: 1,      
-        backgroundColor: '#D93600',
-        alignItems: 'center',
-        justifyContent:'center'  
-       } 
-    })
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+ 
+export default function AppForms() {
+  return (
+    <View style={styles.container}>
+      <Text>Form!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- AppList.js
+++ AppList.js
@@ -1,20 +1,21 @@
-import { StatusBar } from "expo-status-bar"; 
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-export default function AppList(){
-    return(
-        <View style = {style.container}>
-            <Text>Lista!</Text>
-            <StatusBar style="light"/>
-        </View>
-    );
-    const style = StyleSheet({
-       container: {
-        flex: 1,      
-        backgroundColor: '#D93600',
-        alignItems: 'center',
-        justifyContent:'center'  
-       } 
-    })
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+ 
+export default function AppList() {
+  return (
+    <View style={styles.container}>
+      <Text>List!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
 }
-
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
