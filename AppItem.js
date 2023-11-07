42ab3c1d28da4683c5746d2248897128d2cb32a1
Luana CristinaCroft <luanacrystinne@msn.com>
2023-11-07 20:56:51 -0300

    adicionando o que foi implementado hoje na aula

--- AppItem.js
+++ AppItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Stylesheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function AppItem(props){
     return(
@@ -16,3 +16,51 @@ export default function AppItem(props){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      marginTop: 20,
+      width: '100%'
+    },
+    buttonsContainer: {
+        flexDirection: 'row-reverse',
+        alignItems: 'flex-end',
+        borderBottomWidth: 1,
+        borderBottomColor: '#CCC',
+        paddingBottom: 10,
+        marginTop: 10,
+    },
+    editButton: {
+        marginLeft: 10,
+        height: 40,
+        backgroundColor: 'blue',
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 12,
+        elevation: 10,
+        shadowOpacity: 10,
+        shadowColor: '#ccc',
+        alignItems: 'center'
+    },
+    deleteButton: {
+        marginLeft: 10,
+        height: 40,
+        width: 40,
+        backgroundColor: 'red',
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 12,
+        elevation: 10,
+        shadowOpacity: 10,
+        shadowColor: '#ccc',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    textItem: {
+        fontSize: 20,
+    }
+  });
